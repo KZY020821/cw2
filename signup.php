12df59732d0c79eb53ff2695770aa2594b253438
Re Xuan <rexuan02@gmail.com>
2023-03-23 11:09:27 +0800

    done backend

---
+++ signup.php
@@ -0,0 +1,103 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Retrieve form data
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
+
+  // Validate form data
+  $errors = array();
+
+  if (empty($username)) {
+    $errors[] = "Username is required";
+  }
+
+  if (empty($email)) {
+    $errors[] = "Email is required";
+  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = "Invalid email format";
+  }
+
+  if (empty($password)) {
+    $errors[] = "Password is required";
+  } elseif (strlen($password) < 8) {
+    $errors[] = "Password must be at least 8 characters long";
+  }
+
+  if ($password != $confirm_password) {
+    $errors[] = "Passwords do not match";
+  }
+
+  // If there are no errors, insert user data into database
+  if (empty($errors)) {
+    $dbServerName = "localhost";
+    $dbUserName = "root";
+    $dbPassword = "";
+    $dbName = "5614ycom_cw2";
+
+    // Connect to database
+    $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Prepare SQL statement to insert user data into database
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    // Execute SQL statement
+    if (mysqli_query($conn, $sql)) {
+      // Registration successful, redirect user to login page
+      header("Location: login.php");
+      exit();
+    } 
+    
+    else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    // Close database connection
+    mysqli_close($conn);
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign up</title>
+</head>
+
+<body>
+  <?php if (!empty($errors)): ?>
+    <ul>
+      <?php foreach ($errors as $error): ?>
+        <li><?php echo $error; ?></li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" value="<?php echo isset($username) ? $username : ''; ?>"><br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" value="<?php echo isset($email) ? $email : ''; ?>"><br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password"><br><br>
+
+    <label for="confirm_password">Confirm password:</label>
+    <input type="password" id="confirm_password" name="confirm_password"><br><br>
+
+    <button type="submit">Sign up</button>
+  </form>
+</body>
+
+</html>
